6d714e1dbfbe526283747dbe902121e8ace4d045
anirudhthakur <67862243+anirudhthakur@users.noreply.github.com>
2022-06-11 22:10:37 +0530

    library

---
+++ sort.h
@@ -0,0 +1,14 @@
+
+void q_sort(void **, int, int , int (*)(void *, void *));
+
+void swap(void **, int, int);
+
+void merge_sort(void **, int , int , void **, int (*)(void *, void *));
+
+void bubble_sort(void ** , int, int (*)(void *, void *));
+
+void insertion_sort(void **, int, int(*)(void *, void *));
+
+int linear_search(void *, void *, int,  int(*)(void *, void * , int ));
+
+int binary_search(void *, void *, int, int, int (*)(void *, void *, int));
